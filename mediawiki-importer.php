45758a9c5befd51cdc5961ee56ce933fd68c74d5
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-08-09 21:43:03 +0530

    description and license information

---
+++ mediawiki-importer.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Mediawiki Importer
+Plugin URI:
+Description: Import Mediawiki content to WordPress.
+Author: Prasath Nadarajah
+Author URI:
+Version: 0.1
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
