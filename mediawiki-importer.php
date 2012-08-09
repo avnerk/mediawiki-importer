3d514ab75bc2b37f43341be318b7d0c587af2902
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-08-09 21:48:36 +0530

    loading importer API. defining new class Mediawiki_Import

--- mediawiki-importer.php
+++ mediawiki-importer.php
@@ -8,3 +8,15 @@ Author URI:
 Version: 0.1
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
+
+if ( !defined('WP_LOAD_IMPORTERS') )
+	return;
+
+// Load Importer API
+require_once ABSPATH . 'wp-admin/includes/import.php';
+
+class Mediawiki_Import {
+
+}
+
+$mediawiki_import = new Mediawiki_Import();
