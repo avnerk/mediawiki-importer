29e600c8e5c9bdc58c955d8d469797cc95cbb7e6
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-08-09 21:50:42 +0530

    importing class wp importer

--- mediawiki-importer.php
+++ mediawiki-importer.php
@@ -15,6 +15,12 @@ if ( !defined('WP_LOAD_IMPORTERS') )
 // Load Importer API
 require_once ABSPATH . 'wp-admin/includes/import.php';
 
+if ( !class_exists( 'WP_Importer' ) ) {
+	$class_wp_importer = ABSPATH . 'wp-admin/includes/class-wp-importer.php';
+	if ( file_exists( $class_wp_importer ) )
+		require_once $class_wp_importer;
+}
+
 class Mediawiki_Import {
 
 }
